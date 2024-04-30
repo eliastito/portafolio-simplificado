658a73a3c8db97d7c2672ba36fd324ac4938f3df
unknown <eliastito414@gmail.com>
2024-04-30 11:11:20 -0300

    Primer commit

---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+/* 
+    objetivo 1 - quando o usuario clicar no botão de mostrar mais deve abrir os projetos
+    que estão escondidos no html
+
+       Passo 1 pegar o botão mostrar mais no JS pra poder verrificar quando o usuário 
+       clicar  em cima dele
+
+       Passo 2 - identificar o clique no botão
+
+        Passo 3 - adicionar a classe "ativo" nos projetos escondidos 
+    
+    objetivo 2 - esconder o botão de mostrar mais 
+         Passo 1 - pegar o botão e esconder ele       
+ */
+
+// objetivo 2 - quando o usuario clicar no botão de mostrar mais  deve abrir os projetos que  estão escondidos no html 
+
+    // Passo 1 - pegar o botão mostrar mais no JS pra podder verificar quando o usuarios clicar em cima dele 
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+}); 
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
+
+function mostrarProjetos(){
+    projetosInativos.forEach(projetosInativo =>{
+        projetosInativo.classList.add('ativo');
+     });
+}
+
+      
+
+
+  
+
+    
+
+
+
